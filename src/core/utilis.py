653e18d9d19feff02eb3000e9a6eaa90a55ec79d
Jeanpierre-c-coder <kondejean3@gmail.com>
2026-05-23 12:54:07 +0200

    Fix return statement in clean_text function

--- src/core/utilis.py
+++ src/core/utilis.py
@@ -5,8 +5,7 @@ from typing import Optional
 def clean_text(text: Optional[str]) -> str:
    
     if not isinstance(text, str):
-        return ""
-
+        return "
     text = text.lower()
 
     text = unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode()
